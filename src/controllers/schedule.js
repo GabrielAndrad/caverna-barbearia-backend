75bbd3648dacef6de2db67c74f7b2995844a0041
gabriel_andrad <gabriel.nunes@tegra.com.br>
2022-10-26 23:09:21 -0300

    teste

--- src/controllers/schedule.js
+++ src/controllers/schedule.js
@@ -62,18 +62,16 @@ router.get('/schedule-hours/:date',async(req,res) => {
 
      })
      return res.send(setHours)
-  } catch(err){
+  } catch(err){ 
     return res.send(hours)
   }
 })
 //return all schedules
 router.get('/schedule', async(req,res) => {
-  // console.log(schedule)
   try{
-    // const schedules = await schedule.find()
-  
-    // console.log(schedules)
-    return res.send([])
+    const schedules = await schedule.find()
+
+    return res.send(schedules)
   } catch(err){
     console.log(err)
     res.status(400).send(err)
